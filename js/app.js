de016806c6460b9bbed485719de7e78dadc0bdf6
bangiyan <bangiyan@bangiyan.lan>
2023-10-02 20:06:13 +0700

    add fetching

--- js/app.js
+++ js/app.js
@@ -1,170 +1,64 @@
-function getCurrentUrl() {
-    const url = location.href.split('?').shift()
-
-    if (url.match(/\/$/)) {
-        return url.replace(/\/$/, '')
-    }
-
-    if (url.match(/index\.html$/)) {
-        return url.replace(/index\.html$/, '')
-    }
-
-    return url
+async function fetchData(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
 }
 
-const el = new Vue({
-    el: '#app',
-    data: {
-        baseApiUrl: getCurrentUrl() + '/api',
-        provinces: [],
-        regencies: [],
-        districts: [],
-        villages: [],
-        fetchingProvinces: false,
-        fetchingRegencies: false,
-        fetchingDistricts: false,
-        fetchingVillages: false,
-        provinceId: '',
-        regencyId: '',
-        districtId: '',
-        villageId: '',
-        completed: false,
-    },
-    watch: {
-        provinceId() {
-            this.regencyId = ''
-            this.districtId = ''
-            this.villageId = ''
-            this.fetchRegencies()
-        },
-        regencyId() {
-            this.districtId = ''
-            this.villageId = ''
-            this.fetchDistricts()
-        },
-        districtId() {
-            this.villageId = ''
-            this.fetchVillages()
-        },
-        villageId() {
-            this.completed = true
-        }
-    },
-    computed: {
-        urlApiProvinces() {
-            return `${this.baseApiUrl}/provinces.json`
-        },
-        urlApiRegencies() {
-            return `${this.baseApiUrl}/regencies/${this.provinceId}.json`
-        },
-        urlApiDistricts() {
-            return `${this.baseApiUrl}/districts/${this.regencyId}.json`
-        },
-        urlApiVillages() {
-            return `${this.baseApiUrl}/villages/${this.districtId}.json`
-        },
-        fetchProvincesCode() {
-            return [
-                `fetch(\`<a href="${this.urlApiProvinces}" target="_blank">${this.urlApiProvinces}</a>\`)`,
-                '.then(response => response.json())',
-                '.then(provinces => console.log(provinces));'
-            ].join('\n')
-        },
-        fetchRegenciesCode() {
-            return !this.provinceId ? '' : [
-                `<small class="text-fade-50">// ID Provinsi ${this.selectedProvince.name} = ${this.provinceId}</small>`,
-                `fetch(\`<a href="${this.urlApiRegencies}" target="_blank">${this.urlApiRegencies}</a>\`)`,
-                '.then(response => response.json())',
-                '.then(regencies => console.log(regencies));'
-            ].join('\n')
-        },
-        fetchDistrictsCode() {
-            return !this.regencyId ? '' : [
-                `<small class="text-fade-50">// ID ${this.selectedRegency.name} = ${this.regencyId}</small>`,
-                `fetch(\`<a href="${this.urlApiDistricts}" target="_blank">${this.urlApiDistricts}</a>\`)`,
-                '.then(response => response.json())',
-                '.then(districts => console.log(districts));'
-            ].join('\n')
-        },
-        fetchVillagesCode() {
-            return !this.districtId ? '' : [
-                `<small class="text-fade-50">// ID Kecamatan ${this.selectedDistrict.name} = ${this.districtId}</small>`,
-                `fetch(\`<a href="${this.urlApiVillages}" target="_blank">${this.urlApiVillages}</a>\`)`,
-                '.then(response => response.json())',
-                '.then(villages => console.log(villages));'
-            ].join('\n')
-        },
-        selectedProvince() {
-            return this.provinces.find(item => item.id == this.provinceId)
-        },
-        selectedRegency() {
-            return this.regencies.find(item => item.id == this.regencyId)
-        },
-        selectedDistrict() {
-            return this.districts.find(item => item.id == this.districtId)
-        },
-        selectedVillage() {
-            return this.villages.find(item => item.id == this.villageId)
-        },
-        responseProvinces() {
-            return JSON.stringify(this.provinces, null, 2)
-        },
-        responseRegencies() {
-            return JSON.stringify(this.regencies, null, 2)
-        },
-        responseDistricts() {
-            return JSON.stringify(this.districts, null, 2)
-        },
-        responseVillages() {
-            return JSON.stringify(this.villages, null, 2)
-        },
-    },
-    created() {
-        this.fetchProvinces()
-    },
-    mounted() {
-        [...document.querySelectorAll('.unhide-on-mounted')].forEach(el => {
-            el.classList.remove('d-none')
-        })
-    },
-    methods: {
-        async fetchProvinces() {
-            this.fetchingProvinces = true
-            const result = await fetch(`api/provinces.json`)
-            this.fetchingProvinces = false
-            this.provinces = await result.json()
-        },
-        async fetchRegencies() {
-            if (!this.provinceId) {
-                this.regencies = []
-                return
-            }
-            this.fetchingRegencies = true
-            const result = await fetch(`api/regencies/${this.provinceId}.json`)
-            this.fetchingRegencies = false
-            this.regencies = await result.json()
-        },
-        async fetchDistricts() {
-            if (!this.regencyId) {
-                this.districts = []
-                return
-            }
+async function populateSelect(elementId, apiUrl) {
+    const selectElement = document.getElementById(elementId)
+    const data = await fetchData(apiUrl)
 
-            this.fetchingDistricts = true
-            const result = await fetch(`api/districts/${this.regencyId}.json`)
-            this.fetchingDistricts = false
-            this.districts = await result.json()
-        },
-        async fetchVillages() {
-            if (!this.districtId) {
-                this.villages = []
-                return
-            }
+    data.forEach((item) => {
+        const option = document.createElement("option")
+        option.value = item.id
+        option.text = item.name
+        selectElement.appendChild(option)
+    })
+}
 
-            this.fetchingVillages = true
-            const result = await fetch(`api/villages/${this.districtId}.json`)
-            this.fetchingVillages = false
-            this.villages = await result.json()
-        }
+async function populateProvinces() {
+    await populateSelect("provinceId", "api/provinces.json")
+}
+
+async function populateRegencies() {
+    const provinceId = document.getElementById("provinceId").value
+    if (provinceId) {
+        await populateSelect("regencyId", `api/regencies/${provinceId}.json`)
     }
+}
+
+async function populateDistricts() {
+    const regencyId = document.getElementById("regencyId").value
+    if (regencyId) {
+        await populateSelect("districtId", `api/districts/${regencyId}.json`)
+    }
+}
+
+async function populateVillages() {
+    const districtId = document.getElementById("districtId").value
+    if (districtId) {
+        await populateSelect("villageId", `api/villages/${districtId}.json`)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    populateProvinces()
+
+    document.getElementById("provinceId").addEventListener("change", () => {
+        document.getElementById("regencyId").innerHTML = "<option value=''></option>"
+        document.getElementById("districtId").innerHTML = "<option value=''></option>"
+        document.getElementById("villageId").innerHTML = "<option value=''></option>"
+        populateRegencies()
+    })
+
+    document.getElementById("regencyId").addEventListener("change", () => {
+        document.getElementById("districtId").innerHTML = "<option value=''></option>"
+        document.getElementById("villageId").innerHTML = "<option value=''></option>"
+        populateDistricts()
+    })
+
+    document.getElementById("districtId").addEventListener("change", () => {
+        document.getElementById("villageId").innerHTML = "<option value=''></option>"
+        populateVillages()
+    })
 })
